9d9aec0885a8fe8bc48f314101bd4e2e1ffc2b46
nelvin94 <sitebrechko@gmail.com>
2022-11-04 23:50:26 +0200

    4.11.22 (5 часов курса из 10)

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
-let user = {};
-a = !!user
-console.log(a)
-console.log(!!user)
+/** //Тест результата функции "или", которая ищет первое истинное значение
+a = '' || 0 || '44'
+//console.log(!!a)
+
+
+
+//Тест разделения объекта на свойства, чтобы добавить его значения, в новый объект
+let b ={
+    color: 'red',
+    c:5,
+    hk: 'hhh'
+}
+//console.log(b)
+
+const c= {
+    ...b,
+    ololo: 7,
+    c:15
+}
+//console.table(c)
+//instanceof(b)
+
+
+
+//Тест шаблонной строки 
+let name = 'Дима'
+let city = 'Ирпене'
+let result = `Меня зовут ${name}, я живу в ${city}`
+//console.log(result)
+//console.log(`меня зовут ${name}, я живу в ${city}`)
+
+
+
+// Тест стандартной функции со значением по умолчанию
+function defaulttest(a, b = 1, c = 3) {
+    let g
+    ++a
+    a++
+    g = a * b
+    console.log(g)
+    return g * c
+}
+//console.log(defaulttest (1, 2, 4))
+//console.log(defaulttest (1, 2))
+
+//Тоже самое, но написано с использованием анонимного функционального выражение
+const defaulttest = function(a, b = 1, c = 3) {
+    let g
+    ++a
+    a++
+    g = a * b
+    console.log(g)
+    return g * c
+}
+console.log(defaulttest (1, 2, 4))
+console.log(defaulttest (1, 2))
+
+//Тоже самое, но написано с использованием стрелочной функции
+const defaulttest = (a, b = 1, c = 3) => {
+    let g
+    ++a
+    a++
+    g = a * b
+    console.log(g)
+    return g * c
+}
+console.log(defaulttest (1, 2, 4))
+console.log(defaulttest (1, 2))
+*/
